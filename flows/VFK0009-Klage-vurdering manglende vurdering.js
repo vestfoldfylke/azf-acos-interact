b1a5bc197649e45ef2914d277e2b8cbb7c459213
Nils Krane Thvedt <nils.thvedt@vtfk.no>
2023-11-30 15:43:05 +0100

    new flows

---
+++ flows/VFK0009-Klage-vurdering manglende vurdering.js
@@ -0,0 +1,180 @@
+const description = 'Sender til elevmappe'
+const { nodeEnv } = require('../config')
+const title = 'Klage - vurdering/manglende vurdering'
+const { schoolInfo } = require('../lib/data-sources/vfk-schools')
+module.exports = {
+  config: {
+    enabled: true,
+    doNotRemoveBlobs: true
+  },
+  parseXml: {
+    enabled: true,
+    options: {
+    }
+  },
+
+  // Synkroniser elevmappe
+  syncElevmappe: {
+    enabled: true,
+    options: {
+      /*
+      condition: (flowStatus) => { // use this if you only need to archive some of the forms.
+        return flowStatus.parseXml.result.ArchiveData.TilArkiv === 'true'
+      },
+      */
+      mapper: (flowStatus) => { // for å opprette person basert på fødselsnummer
+        // Mapping av verdier fra XML-avleveringsfil fra Acos.
+        return {
+          ssn: flowStatus.parseXml.result.ArchiveData.Fnr
+        }
+      }
+    }
+  },
+
+  // Arkiverer dokumentet i elevmappa
+  archive: { // archive må kjøres for å kunne kjøre signOff (noe annet gir ikke mening)
+    enabled: true,
+    options: {
+      /*
+      condition: (flowStatus) => { // use this if you only need to archive some of the forms.
+        return flowStatus.parseXml.result.ArchiveData.TilArkiv === 'true'
+      },
+      */
+      mapper: (flowStatus, base64, attachments) => {
+        const xmlData = flowStatus.parseXml.result.ArchiveData
+        const elevmappe = flowStatus.syncElevmappe.result.elevmappe
+        const p360Attachments = attachments.map(att => {
+          return {
+            Base64Data: att.base64,
+            Format: att.format,
+            Status: 'F',
+            Title: att.title,
+            VersionFormat: att.versionFormat
+          }
+        })
+        const documentData = {
+          service: 'DocumentService',
+          method: 'CreateDocument',
+          parameter: {
+            AccessCode: '13',
+            Category: 'Dokument inn',
+            Contacts: [
+              {
+                ReferenceNumber: xmlData.Fnr,
+                Role: 'Avsender',
+                IsUnofficial: true
+              }
+            ],
+            DocumentDate: new Date().toISOString(),
+            Files: [
+              {
+                Base64Data: base64,
+                Category: '1',
+                Format: 'pdf',
+                Status: 'F',
+                Title: title,
+                VersionFormat: 'A'
+              },
+              ...p360Attachments
+            ],
+            Paragraph: 'Offl. § 13 jf. fvl. § 13 (1) nr.1',
+            Status: 'J',
+            Title: title,
+            // UnofficialTitle: '',
+            Archive: 'Sensitivt elevdokument',
+            CaseNumber: elevmappe.CaseNumber
+          }
+        }
+
+        if (xmlData.Egendefinert1 === 'Privatist') {
+          documentData.parameter.ResponsibleEnterpriseRecno = nodeEnv === 'production' ? '200015' : '200018' // Seksjon Sektorstøtte, inntak og eksamen
+          documentData.parameter.AccessGroup = 'Eksamen'
+        } else if (xmlData.Egendefinert1 === 'Elev') {
+          const school = schoolInfo.find(school => school.orgNr.toString() === xmlData.SkoleOrgNr)
+          if (!school) throw new Error(`Could not find any school with orgNr: ${xmlData.SkoleOrgNr}`)
+          documentData.parameter.ResponsibleEnterpriseNumber = xmlData.SkoleOrgNr
+          documentData.parameter.AccessGroup = school.tilgangsgruppe
+        } else if (xmlData.Egendefinert1 === 'Lærling/lærekandidat/praksiskandidat') {
+          documentData.parameter.ResponsibleEnterpriseRecno = nodeEnv === 'production' ? '200016' : '200019' // Seksjon Fag og yrkesopplæring // denne er ikke verifisert
+          documentData.parameter.AccessGroup = 'Fagopplæring'
+        } else if (xmlData.Egendefinert1 === 'Voksen (Kompetansebyggeren)') {
+          documentData.parameter.ResponsibleEnterpriseRecno = nodeEnv === 'production' ? '200037' : '200045' // Kompetansebyggeren // denne er ikke verifisert
+          documentData.parameter.AccessGroup = 'Elev Kompetansebyggeren'
+        } else {
+          throw new Error('Fikk ukjent verdi inn i Egendefinert1 fra skjemaets xml-fil. Trenger "Privatist", "Elev", "Lærling/lærekandidat/praksiskandidat" eller "Voksen (Kompetansebyggeren)"')
+        }
+        console.log(documentData)
+        return documentData
+      }
+    }
+
+  },
+
+  signOff: {
+    enabled: false
+  },
+
+  closeCase: {
+    enabled: false
+  },
+  /*
+  sharepointList: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => {
+        const xmlData = flowStatus.parseXml.result.ArchiveData
+        if (!xmlData.Postnr) throw new Error('Postnr har ikke kommet med fra XML')
+        return [
+          {
+            siteId: '0a4121ce-7384-474c-afff-ee20f48bff5e',
+            path: 'sites/BDK-Jrgensteste-team/Lists/ACOS%20test%20%20Bestilling%20av%20dokumentasjon%20for%20privati/AllItems.aspx',
+            listId: 'D1085908-9111-4b6d-84d3-fc8ecd29d398',
+            uploadFormPdf: true,
+            uploadFormAttachments: true,
+            fields: {
+              Title: xmlData.Fnr || 'Mangler fnr', // husk å bruke internal name på kolonnen
+              Fornavn: xmlData.Fornavn,
+              Etternavn: xmlData.Etternavn,
+              Adresse: xmlData.Adresse,
+              Postnummerogsted: `${xmlData.Postnr} ${xmlData.Poststed}`,
+              Mobil: xmlData.Mobil,
+              E_x002d_postadresse: xmlData.Epost,
+              Typedokumentasjon: xmlData.TypeDok,
+              Typeautorasisjon: xmlData.TypeAut,
+              Eksamenssted: xmlData.Eksamenssted,
+              Fag: xmlData.Fag,
+              _x00d8_nsketmottak: xmlData.OnsketMottak,
+              Eksamensperiode: xmlData.AarSemester,
+              Alternativadresse: xmlData.AltAdresse
+
+            }
+          }
+        ]
+      }
+    }
+  },
+  */
+
+  statistics: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => {
+        // const xmlData = flowStatus.parseXml.result.ArchiveData
+        // Mapping av verdier fra XML-avleveringsfil fra Acos. Alle properties under må fylles ut og ha verdier
+        return {
+          company: 'Opplæring',
+          department: 'FAGOPPLÆRING',
+          description,
+          type: title, // Required. A short searchable type-name that distinguishes the statistic element
+          // optional fields:
+          tilArkiv: flowStatus.parseXml.result.ArchiveData.TilArkiv,
+          documentNumber: flowStatus.archive?.result?.DocumentNumber || 'tilArkiv er false' // Optional. anything you like
+        }
+      }
+    }
+  },
+
+  failOnPurpose: {
+    enabled: false
+  }
+}
