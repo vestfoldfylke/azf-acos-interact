83f56b6bae8de8f27f4c0f054ff5bb7714f758ae
Nils Krane Thvedt <nils.thvedt@vtfk.no>
2023-12-10 21:29:40 +0100

    new flow files

---
+++ flows/VFK0232-Nettskolen - matematikkurs P-PY for opptak til lærerutdanning.js
@@ -0,0 +1,184 @@
+const description = 'Sender til elevmappe'
+const { nodeEnv } = require('../config')
+// const { schoolInfo } = require('../lib/data-sources/vfk-schools')
+module.exports = {
+  config: {
+    enabled: true,
+    doNotRemoveBlobs: true
+  },
+  parseXml: {
+    enabled: true,
+    options: {
+    }
+  },
+  /* Felter fra Acos:
+  ArchiveData {
+    string Fnr
+    string Fornavn
+    string Etternavn
+    string Adresse
+    string PostnrSted
+    string Mobilnr
+    string Epost
+    string Fylke
+    string Kurstidspunkt
+    string Karakter1P1PY
+    string Karakter2P2PY
+    string ElevFdato
+    string Fag
+    string Samtykke
+    fagliste[] ValgteFag
+      fagliste{
+        string Fagnavn
+      }
+  }
+
+  */
+
+  // Synkroniser elevmappe
+  syncElevmappe: {
+    enabled: true,
+    options: {
+      /*
+      condition: (flowStatus) => { // use this if you only need to archive some of the forms.
+        return flowStatus.parseXml.result.ArchiveData.TilArkiv === 'true'
+      },
+      */
+      mapper: (flowStatus) => { // for å opprette person basert på fødselsnummer
+        // Mapping av verdier fra XML-avleveringsfil fra Acos.
+        return {
+          ssn: flowStatus.parseXml.result.ArchiveData.Fnr
+        }
+      }
+    }
+  },
+
+  // Arkiverer dokumentet i elevmappa
+  archive: { // archive må kjøres for å kunne kjøre signOff (noe annet gir ikke mening)
+    enabled: true,
+    options: {
+      /*
+      condition: (flowStatus) => { // use this if you only need to archive some of the forms.
+        return flowStatus.parseXml.result.ArchiveData.TilArkiv === 'true'
+      },
+      */
+      mapper: (flowStatus, base64, attachments) => {
+        const xmlData = flowStatus.parseXml.result.ArchiveData
+        const elevmappe = flowStatus.syncElevmappe.result.elevmappe
+        const p360Attachments = attachments.map(att => {
+          return {
+            Base64Data: att.base64,
+            Format: att.format,
+            Status: 'F',
+            Title: att.title,
+            VersionFormat: att.versionFormat
+          }
+        })
+        // const school = schoolInfo.find(school => school.orgNr.toString() === xmlData.SkoleOrgNr)
+        // if (!school) throw new Error(`Could not find any school with orgNr: ${xmlData.SkoleOrgNr}`)
+        const dateList = (new Date().toISOString()).split('-')
+        const year = `${dateList[0]}`
+        return {
+          service: 'DocumentService',
+          method: 'CreateDocument',
+          parameter: {
+            AccessCode: '13',
+            AccessGroup: 'Elev Horten',
+            Category: 'Dokument inn',
+            Contacts: [
+              {
+                ReferenceNumber: xmlData.Fnr,
+                Role: 'Avsender',
+                IsUnofficial: true
+              }
+            ],
+            DocumentDate: new Date().toISOString(),
+            Files: [
+              {
+                Base64Data: base64,
+                Category: '1',
+                Format: 'pdf',
+                Status: 'F',
+                Title: 'Samtykke - Nettskolen',
+                VersionFormat: 'A'
+              },
+              ...p360Attachments
+            ],
+            Paragraph: 'Offl. § 13 jf. fvl. § 13 (1) nr.1',
+            ResponsibleEnterpriseRecno: nodeEnv === 'production' ? '200341' : '200208', // Horten vgs nettskolen
+            // ResponsiblePersonEmail: '',
+            Status: 'J',
+            Title: `Søknad til matematikkurs for opptak til lærerutdanning ${year}`,
+            UnofficialTitle: `Søknad til matematikkurs for opptak til lærerutdanning ${year} - ${xmlData.Fornavn} ${xmlData.Etternavn}`,
+            Archive: 'Elevdokument',
+            CaseNumber: elevmappe.CaseNumber
+          }
+        }
+      }
+    }
+
+  },
+
+  signOff: {
+    enabled: false
+  },
+
+  closeCase: {
+    enabled: false
+  },
+
+  sharepointList: {
+    enabled: false,
+    options: {
+      mapper: (flowStatus) => {
+        const xmlData = flowStatus.parseXml.result.ArchiveData
+        const sharepointElements = []
+        const fagliste = Array.isArray(xmlData.ValgteFag.fagliste) ? xmlData.ValgteFag.fagliste : [xmlData.ValgteFag.fagliste] // Sjekker om det er mer enn ett fag i lista (altså et array). Hvis ikke lag et array med det ene elementet
+        for (const fag of fagliste) {
+          const sharepointElement = {
+            testListUrl: '',
+            prodListUrl: '',
+            uploadFormPdf: true,
+            uploadFormAttachments: true,
+            fields: {
+              Title: xmlData.Etternavn || 'Mangler etternavn', // husk å bruke internal name på kolonnen
+              Fornavns_x00f8_ker: xmlData.Fornavn,
+              Adresse: xmlData.Adresse,
+              Postnummerogsted: xmlData.PostnrSted,
+              Mobilnummer: xmlData.Mobilnr,
+              E_x002d_postadresse: xmlData.Epost,
+              Bostedsfylke: xmlData.Fylke,
+              Kurstidspunkt: xmlData.Kurstidspunkt,
+              Fagvalg: fag.Fagnavn
+            }
+          }
+          sharepointElements.push(sharepointElement)
+        }
+        return sharepointElements
+      }
+    }
+  },
+
+  statistics: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => {
+        // const xmlData = flowStatus.parseXml.result.ArchiveData
+        // Mapping av verdier fra XML-avleveringsfil fra Acos. Alle properties under må fylles ut og ha verdier
+        return {
+          company: 'Opplæring',
+          department: 'Nettskolen',
+          description,
+          type: 'Nettskolen - Søknad til matematikkurs for opptak til lærerutdanning', // Required. A short searchable type-name that distinguishes the statistic element
+          // optional fields:
+          // tilArkiv: flowStatus.parseXml.result.ArchiveData.TilArkiv,
+          documentNumber: flowStatus.archive?.result?.DocumentNumber // || 'tilArkiv er false' // Optional. anything you like
+        }
+      }
+    }
+  },
+
+  failOnPurpose: {
+    enabled: false
+  }
+}
