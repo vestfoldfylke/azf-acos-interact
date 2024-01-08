4b5d5e4e6bac5cf18633b8556fdbd11df9862e2d
Nils Krane Thvedt <nilskt@gmail.com>
2024-01-08 08:07:45 +0100

    clean-up

--- example-flows/EX004-sync-employee.js
+++ example-flows/EX004-sync-employee.js
@@ -7,20 +7,14 @@ module.exports = {
     options: {
     }
   },
-  syncElevmappe: {
+  syncEmployee: {
     enabled: true,
     options: {
-      mapper: (flowStatus) => {
-        // Mapping av verdier fra XML-avleveringsfil fra Acos. Alle properties under må fylles ut og ha verdier for å opprette elevmappe med fiktivt fødselsnummer
+      mapper: (flowStatus) => { 
+        // Mapping av verdier fra XML-avleveringsfil fra Acos. Alle properties under må fylles ut og ha verdier for å opprette privatperson med fiktivt fødselsnummer
         return {
-          generateFakeSsn: true,
-          birthdate: flowStatus.parseXml.result.ArchiveData.Foedselsdato,
-          gender: flowStatus.parseXml.result.ArchiveData.Kjoenn,
-          firstName: flowStatus.parseXml.result.ArchiveData.Fornavn,
-          lastName: flowStatus.parseXml.result.ArchiveData.Etternavn,
-          streetAddress: flowStatus.parseXml.result.ArchiveData.Adresse,
-          zipCode: flowStatus.parseXml.result.ArchiveData.Postnr,
-          zipPlace: flowStatus.parseXml.result.ArchiveData.Sted
+          ssn: flowStatus.parseXml.result.ArchiveData.Fnr,
+          forceUpdate: true // optional - forces update of privatePerson instead of quick return if it exists
         }
       }
     }
