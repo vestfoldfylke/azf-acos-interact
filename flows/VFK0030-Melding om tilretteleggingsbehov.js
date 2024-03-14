643aa1ef240281711c169bc6f9d237f4cac89815
Nils Krane Thvedt <nilskt@gmail.com>
2024-03-14 09:20:37 +0100

    added sp-list job

--- flows/VFK0030-Melding om tilretteleggingsbehov.js
+++ flows/VFK0030-Melding om tilretteleggingsbehov.js
@@ -98,43 +98,29 @@ module.exports = {
   closeCase: {
     enabled: false
   },
-  /*
   sharepointList: {
     enabled: true,
     options: {
       mapper: (flowStatus) => {
         const xmlData = flowStatus.parseXml.result.ArchiveData
-        if (!xmlData.Postnr) throw new Error('Postnr har ikke kommet med fra XML')
+        // if (!xmlData.Postnr) throw new Error('Postnr har ikke kommet med fra XML')
         return [
           {
-            siteId: '0a4121ce-7384-474c-afff-ee20f48bff5e',
-            path: 'sites/BDK-Jrgensteste-team/Lists/ACOS%20test%20%20Bestilling%20av%20dokumentasjon%20for%20privati/AllItems.aspx',
-            listId: 'D1085908-9111-4b6d-84d3-fc8ecd29d398',
-            uploadFormPdf: true,
-            uploadFormAttachments: true,
+            testListUrl: 'https://vestfoldfylke.sharepoint.com/sites/OPT-Fylkesadministrasjonopplring-Listerinntak/Lists/Melding%20om%20tilretteleggingsbehov/AllItems.aspx',
+            prodListUrl: 'https://vestfoldfylke.sharepoint.com/sites/OPT-Fylkesadministrasjonopplring-Listerinntak/Lists/Melding%20om%20tilretteleggingsbehov/AllItems.aspx',
+            uploadFormPdf: false,
+            uploadFormAttachments: false,
             fields: {
-              Title: xmlData.Fnr || 'Mangler fnr', // husk å bruke internal name på kolonnen
+              Title: xmlData.Fnr, // husk å bruke internal name på kolonnen
               Fornavn: xmlData.Fornavn,
               Etternavn: xmlData.Etternavn,
-              Adresse: xmlData.Adresse,
-              Postnummerogsted: `${xmlData.Postnr} ${xmlData.Poststed}`,
-              Mobil: xmlData.Mobil,
-              E_x002d_postadresse: xmlData.Epost,
-              Typedokumentasjon: xmlData.TypeDok,
-              Typeautorasisjon: xmlData.TypeAut,
-              Eksamenssted: xmlData.Eksamenssted,
-              Fag: xmlData.Fag,
-              _x00d8_nsketmottak: xmlData.OnsketMottak,
-              Eksamensperiode: xmlData.AarSemester,
-              Alternativadresse: xmlData.AltAdresse
-
+              _x00d8_nskertilrettelegging: xmlData.Egendefinert1
             }
           }
         ]
       }
     }
   },
-  */
 
   statistics: {
     enabled: true,
