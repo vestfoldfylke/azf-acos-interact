d1898bc67af4735a0f5b7ab406f84b30d1815d31
Nils Krane Thvedt <nilskt@gmail.com>
2024-11-19 10:02:20 +0100

    new flow files

---
+++ flows/VFK0309-Tannhelse - brukertilganger P360.js
@@ -0,0 +1,72 @@
+const description = 'Oppdaterer rad i SP liste.'
+// const { nodeEnv } = require('../config')
+module.exports = {
+  config: {
+    enabled: true,
+    doNotRemoveBlobs: false
+  },
+  parseXml: {
+    enabled: true,
+    options: {
+    }
+  },
+  sharepointGetListItem: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => {
+        const xmlData = flowStatus.parseXml.result.ArchiveData
+        return {
+          testListUrl: 'https://vestfoldfylke.sharepoint.com/sites/OPT-TAN-Tannhelse/Lists/Tilgangsbestillinger/AllItems.aspx',
+          prodListUrl: 'https://vestfoldfylke.sharepoint.com/sites/OPT-TAN-Tannhelse/Lists/Tilgangsbestillinger/AllItems.aspx',
+          searchFilter: `fields/guid0 eq '${xmlData.guid}'` // guid blir sendt med fra første skjema og lagret i lista. Denne raden søker vi etter her
+        }
+      }
+    }
+  },
+  sharepointList: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => {
+        const xmlData = flowStatus.parseXml.result.ArchiveData
+        if (flowStatus.sharepointGetListItem.result.length !== 1) throw new Error('Fant ikke unik match i lista når vi kjørte sharepointGetListItem, sjekk searchFilter i jobben eller plukk ut korrekt id i flowStatus-fila')
+        const id = flowStatus.sharepointGetListItem.result[0]
+        return [
+          {
+            testListUrl: 'https://vestfoldfylke.sharepoint.com/sites/OPT-TAN-Tannhelse/Lists/Tilgangsbestillinger/AllItems.aspx',
+            prodListUrl: 'https://vestfoldfylke.sharepoint.com/sites/OPT-TAN-Tannhelse/Lists/Tilgangsbestillinger/AllItems.aspx',
+            testItemId: id,
+            prodItemId: id,
+            uploadFormPdf: false,
+            uploadFormAttachments: false,
+            fields: {
+              Tilgang_x0020_til_x0020_360: `Utført (${xmlData.utfortAvTidspunkt})`
+            }
+          }
+        ]
+      }
+    }
+  },
+
+  statistics: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => {
+        // const xmlData = flowStatus.parseXml.result.ArchiveData
+        // Mapping av verdier fra XML-avleveringsfil fra Acos. Alle properties under må fylles ut og ha verdier
+        return {
+          company: 'Tannhelse',
+          department: 'Tannhelse',
+          description,
+          type: 'Tannhelse - bestilling av tilganger - P360' // Required. A short searchable type-name that distinguishes the statistic element
+          // optional fields:
+          // tilArkiv: flowStatus.parseXml.result.ArchiveData.TilArkiv,
+          // documentNumber: flowStatus.archive?.result?.DocumentNumber // || 'tilArkiv er false' // Optional. anything you like
+        }
+      }
+    }
+  },
+
+  failOnPurpose: {
+    enabled: false
+  }
+}
