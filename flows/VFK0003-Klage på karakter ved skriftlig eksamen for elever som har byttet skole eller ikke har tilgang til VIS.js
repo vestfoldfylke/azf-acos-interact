7142799e081fd992be51c0d7db4597b8fa221e86
Nils Krane Thvedt <nilskt@gmail.com>
2025-01-14 11:56:56 +0100

    Added archiving

--- flows/VFK0003-Klage på karakter ved skriftlig eksamen for elever som har byttet skole eller ikke har tilgang til VIS.js
+++ flows/VFK0003-Klage på karakter ved skriftlig eksamen for elever som har byttet skole eller ikke har tilgang til VIS.js
@@ -1,4 +1,5 @@
-const description = 'Sender til Sharepoint'
+const description = 'Sender til Elevmappe og SharePoint'
+const { schoolInfo } = require('../lib/data-sources/vfk-schools')
 // const { nodeEnv } = require('../config')
 
 module.exports = {
@@ -27,7 +28,95 @@ module.exports = {
     string Fagkoder
 }
   */
+  // Synkroniser elevmappe
+  syncElevmappe: {
+    enabled: true,
+    options: {
+      /*
+      condition: (flowStatus) => { // use this if you only need to archive some of the forms.
+        return flowStatus.parseXml.result.ArchiveData.TilArkiv === 'true'
+      },
+      */
+      mapper: (flowStatus) => { // for å opprette person basert på fødselsnummer
+        // Mapping av verdier fra XML-avleveringsfil fra Acos.
+        return {
+          ssn: flowStatus.parseXml.result.ArchiveData.Fnr
+        }
+      }
+    }
+  },
 
+  // Arkiverer dokumentet i elevmappa
+  archive: { // archive må kjøres for å kunne kjøre signOff (noe annet gir ikke mening)
+    enabled: true,
+    options: {
+      /*
+      condition: (flowStatus) => { // use this if you only need to archive some of the forms.
+        return flowStatus.parseXml.result.ArchiveData.TilArkiv === 'true'
+      },
+      */
+      mapper: (flowStatus, base64, attachments) => {
+        const xmlData = flowStatus.parseXml.result.ArchiveData
+        const elevmappe = flowStatus.syncElevmappe.result.elevmappe
+        const school = schoolInfo.find(school => school.orgNr.toString() === xmlData.SkoleOrgNr)
+        if (!school) throw new Error(`Could not find any school with orgNr: ${xmlData.SkoleOrgNr}`)
+        const p360Attachments = attachments.map(att => {
+          return {
+            Base64Data: att.base64,
+            Format: att.format,
+            Status: 'F',
+            Title: att.title,
+            VersionFormat: att.versionFormat
+          }
+        })
+        return {
+          service: 'DocumentService',
+          method: 'CreateDocument',
+          parameter: {
+            AccessCode: '13',
+            AccessGroup: school.tilgangsgruppe,
+            Category: 'Dokument inn',
+            Contacts: [
+              {
+                ReferenceNumber: xmlData.Fnr,
+                Role: 'Avsender',
+                IsUnofficial: true
+              }
+            ],
+            DocumentDate: new Date().toISOString(),
+            Files: [
+              {
+                Base64Data: base64,
+                Category: '1',
+                Format: 'pdf',
+                Status: 'F',
+                Title: 'Klage på karakter ved skriftlig eksamen',
+                VersionFormat: 'A'
+              },
+              ...p360Attachments
+            ],
+            Paragraph: 'Offl. § 13 jf. fvl. § 13 (1) nr.1',
+            ResponsibleEnterpriseNumber: xmlData.SkoleOrgnr,
+            // ResponsiblePersonEmail: '',
+            Status: 'J',
+            Title: 'Klage på karakter ved skriftlig eksamen',
+            // UnofficialTitle: '',
+            Archive: 'Elevdokument',
+            CaseNumber: elevmappe.CaseNumber
+          }
+        }
+      }
+    }
+
+  },
+
+  signOff: {
+    enabled: false
+  },
+
+  closeCase: {
+    enabled: false
+  },
   sharepointList: {
     enabled: true,
     options: {
@@ -66,10 +155,10 @@ module.exports = {
           company: 'Utdanning',
           department: 'Eksamen',
           description,
-          type: 'Klage på skriftlig eksamen for elever som har byttet skole eller ikke har tilgang til VIS' // Required. A short searchable type-name that distinguishes the statistic element
+          type: 'Klage på skriftlig eksamen for elever som har byttet skole eller ikke har tilgang til VIS', // Required. A short searchable type-name that distinguishes the statistic element
           // optional fields:
           // tilArkiv: flowStatus.parseXml.result.ArchiveData.TilArkiv,
-          // documentNumber: flowStatus.archive?.result?.DocumentNumber || 'tilArkiv er false' // Optional. anything you like
+          documentNumber: flowStatus.archive?.result?.DocumentNumber || 'tilArkiv er false' // Optional. anything you like
         }
       }
     }
