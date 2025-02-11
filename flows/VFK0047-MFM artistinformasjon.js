8ff0d6ddc0240f1d30eb963dfc2e94b01325cd7c
Nils Krane Thvedt <nilskt@gmail.com>
2025-02-11 09:24:03 +0100

    more logic added

--- flows/VFK0047-MFM artistinformasjon.js
+++ flows/VFK0047-MFM artistinformasjon.js
@@ -11,8 +11,81 @@ module.exports = {
     options: {
     }
   },
-
-  sharepointGetListItem: {
+  sharepointGetListItemProduksjoner: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => {
+        const xmlData = flowStatus.parseXml.result.Produksjon
+        return {
+          testListUrl: 'https://vestfoldfylke.sharepoint.com/sites/SAMU-MarkedforMusikk/Lists/Produksjoner/AllItems.aspx',
+          prodListUrl: 'https://vestfoldfylke.sharepoint.com/sites/SAMU-MarkedforMusikk/Lists/Produksjoner/AllItems.aspx',
+          searchFilter: `fields/ProdID eq '${xmlData.ProdID}'`
+        }
+      }
+    }
+  },
+  
+  sharepointListProduksjoner: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => {
+        const xmlData = flowStatus.parseXml.result.ArchiveData
+        return [
+          {
+            testListUrl: 'https://vestfoldfylke.sharepoint.com/sites/SAMU-MarkedforMusikk/Lists/Produksjoner/AllItems.aspx',
+            prodListUrl: 'https://vestfoldfylke.sharepoint.com/sites/SAMU-MarkedforMusikk/Lists/Produksjoner/AllItems.aspx',
+            uploadFormPdf: true,
+            uploadFormAttachments: false,
+            fields: {
+              Title: xmlData.KonsertNavn || 'Mangler konsertnavn', // husk å bruke internal name på kolonnen
+              Elevpublikum: xmlData.Elevpublikum,
+              M_x00e5_lgruppe: `${xmlData.MalgruppeFra} - ${xmlData.MalgruppeTil}`,
+              Ut_x00f8_vere: xmlData.UtoverOgInstrument
+            }
+          }
+        ]
+      }
+    }
+  },
+  
+  
+  sharepointListKommunikasjon: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => {
+        const xmlData = flowStatus.parseXml.result.ArchiveData
+        return [
+          {
+            testListUrl: 'https://vestfoldfylke.sharepoint.com/sites/SAMU-MarkedforMusikk/Lists/Artistinfokommunikasjon/AllItems.aspx',
+            prodListUrl: 'https://vestfoldfylke.sharepoint.com/sites/SAMU-MarkedforMusikk/Lists/Artistinfokommunikasjon/AllItems.aspx',
+            uploadFormPdf: true,
+            uploadFormAttachments: false,
+            fields: {
+              Title: xmlData.KonsertNavn || 'Mangler etternavn', // husk å bruke internal name på kolonnen
+              Andremerknader: xmlData.TekniskeKrav,
+              Blending: xmlData.Blending,
+              Erkonsertenprodusertform_x00e5_l: xmlData.ProdusertForMalgruppa,
+              Kommentar: xmlData.Kommentar,
+              Konsertlengde_x0028_min_x0029_: xmlData.Konsertlengde,
+              Kontaktperson: xmlData.KontaktNavn,
+              Kortbeskrivelseavproduksjonen: xmlData.Beskrivelse,
+              Link_x0020_til_x0020_nettside: xmlData.LinkWeb,
+              Link_x0020_til_x0020_videoklipp: xmlData.linkVideo,
+              M_x00e5_lgruppealder: `${xmlData.MalgruppeFra} - ${xmlData.MalgruppeTil}`,
+              Navnp_x00e5_produsenter: xmlData.ProdusentNavn,
+              Nedrigg_x0028_min_x0029_: xmlData.NedriggTid,
+              Opprigg_x0028_min_x0029_: xmlData.OppriggTid,
+              Telefon: xmlData.KontaktTlf,
+              Ut_x00f8_vere: xmlData.UtoverOgInstrument,
+              e_x002d_post: xmlData.KontaktEpost
+            }
+          }
+        ]
+      }
+    }
+  },
+/* TRENGS VIRKELIG DENNE???? Antakelig ikke, for det skal jo lages en ny rad med reisefølgedeltaker
+  sharepointGetListItemLogistikk: {
     enabled: true,
     options: {
       mapper: (flowStatus) => {
@@ -25,16 +98,16 @@ module.exports = {
       }
     }
   },
-
-  sharepointList: {
+*/
+  sharepointListLogistikk: {
     enabled: true,
     options: {
       mapper: (flowStatus) => {
         const xmlData = flowStatus.parseXml.result.Produksjon
         const sharepointElements = []
         const reisefolge = Array.isArray(xmlData.Reisefolge) ? xmlData.Reisefolge.Reisefolge : [xmlData.Reisefolge.Reisefolge] // Sjekker om det er mer enn ett fag i lista (altså et array). Hvis ikke lag et array med det ene elementet
-        if (flowStatus.sharepointGetListItem.result.length !== 1) throw new Error('Fant ikke unik match i lista når vi kjørte sharepointGetListItem, sjekk searchFilter i jobben eller plukk ut korrekt id i flowStatus-fila')
-        const id = flowStatus.sharepointGetListItem.result[0]
+        if (flowStatus.sharepointGetListItemLogistikk.result.length !== 1) throw new Error('Fant ikke unik match i lista når vi kjørte sharepointGetListItemLogistikk, sjekk searchFilter i jobben eller plukk ut korrekt id i flowStatus-fila')
+        // const id = flowStatus.SharepointGetListItemLogistikk.result[0]
         for (const person of reisefolge) {
           const sharepointElement = {
             testListUrl: 'https://vestfoldfylke.sharepoint.com/sites/SAMU-MarkedforMusikk/Lists/Logistikkartister/AllItems.aspx',
