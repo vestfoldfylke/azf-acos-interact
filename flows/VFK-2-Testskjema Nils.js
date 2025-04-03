6a739490a7a2e5214e17fb52caf1d5cb46c17cc2
Rune Moskvil Lyngås <rune.moskvil.lyngas@vestfoldfylke.no>
2025-04-03 15:11:56 +0200

    Added possibility for a customJob to wait until another customJob is finished

--- flows/VFK-2-Testskjema Nils.js
+++ flows/VFK-2-Testskjema Nils.js
@@ -7,64 +7,30 @@ module.exports = {
     enabled: true,
     options: {
       mapper: (dialogueInstance) => {
-        if (!dialogueInstance.Testskjema_for_?.Gruppa_øverst?.Fornavn) throw new Error('Missing Gruppa_øverst.Fornavn mangler i JSON filen')
         return {
-          fornavn: dialogueInstance.Testskjema_for_.Gruppa_øverst.Fornavn
+          async: 'streng'
         }
       }
     }
   },
-  groundControl: {
-    enabled: true // Files will be copied to GROUND_CONTROL_STORAGE_ACCOUNT_CONTAINER_NAME, and will be downloaded on local server (./ground-control/index.js)
-  },
-  customJobCreateUtenlandsGreier: () => {
-    // Dytter nødvendig informasjon til utenlandsgreier-blob storage, en annen løsning eller timerTrigger håndterer dette selv
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-  customJobPrepareUtenlandsreise: {
+  customJobDritOgMokk: {
     enabled: true,
-    customJob: async () => {
-      // Hent regiongrupper basert på landkode
-      // Lage lette properties for å sjekke overlappende tidsrom
-      // Skrelle vekk unødvendige properties fra flowStatus (fnr, navn, annen drit)
-      // Denne jobben kjøres da med en gang skjemaet er avlevert, og dumme data vil ikke bli liggende i blobstorage
-    }
-  }
-
-  // funksjon: GetUtlandStuff Lag en funksjon som henter alle regiongruppene, alle tidsrom for brukeren i de ulike regionene.
-
-  customJobAddToGroup: {  
-    enabled: true,
-    runAfterTimestamp: '2023-10-01T00:00:00Z',
-    customJob: async () => {
-      // GetUtlandStuff
-      // Hent alle region-gruppene - finn de gruppene den skal inn i
-      // Legg inn i de den ikke er i allerede
-      // Hva om det er flere overlappende skjemaer i samme regioner?
-      // Legg til brukeren fra skjemaet i en regiongruppe
+    runAfter: 'parseJson',
+    runAfterTimestamp: (jobDef, flowStatus) => {
+      return '2025-04-03T13:06:00Z'
+    },
+    customJob: async (jobDef, flowStatus) => {
+      return 'Drit og Møkk'
     }
   },
-  customJobRemoveFromGroup: {
+  customJobDritOgMokk2: {
     enabled: true,
-    runAfterTimestamp: '2025-10-01T00:00:00Z',
-    customJob: async () => {
-      // GetUtlandStuff
-      // Fjern brukeren fra regiongruppen
-      // He
-      // For hver region den skal ut / inn av
-      // Sjekk om brukeren er i regionen-gruppen
-      // Fjern fra region om den er der
+    runAfter: 'customJobDritOgMokk',
+    runAfterTimestamp: (jobDef, flowStatus) => {
+      return '2025-04-03T13:09:00Z'
+    },
+    customJob: async (jobDef, flowStatus) => {
+      return 'Drit og Møkk 2'
     }
   },
   failOnPurpose: {
