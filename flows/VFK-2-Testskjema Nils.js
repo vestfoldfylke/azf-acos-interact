90a5f4262638f7dce4157f130d9d696b20e0d6dd
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2025-04-03 16:17:09 +0200

    Add options for runAfterTimestamp in custom jobs

--- flows/VFK-2-Testskjema Nils.js
+++ flows/VFK-2-Testskjema Nils.js
@@ -16,8 +16,10 @@ module.exports = {
   customJobDritOgMokk: {
     enabled: true,
     runAfter: 'parseJson',
+    options: {
     runAfterTimestamp: (jobDef, flowStatus) => {
       return '2025-04-03T13:06:00Z'
+      }
     },
     customJob: async (jobDef, flowStatus) => {
       return 'Drit og Møkk'
@@ -26,8 +28,10 @@ module.exports = {
   customJobDritOgMokk2: {
     enabled: true,
     runAfter: 'customJobDritOgMokk',
+    options: {
     runAfterTimestamp: (jobDef, flowStatus) => {
       return '2025-04-03T13:09:00Z'
+      }
     },
     customJob: async (jobDef, flowStatus) => {
       return 'Drit og Møkk 2'
