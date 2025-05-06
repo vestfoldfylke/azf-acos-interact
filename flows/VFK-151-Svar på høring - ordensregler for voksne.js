1c4d79dc7f194358e55ba6d4e447ed93e8c80e69
Nils Krane Thvedt <nilskt@gmail.com>
2025-05-06 13:32:29 +0200

    new flow file

--- flows/VFK-151-Svar på høring - ordensregler for voksne.js
+++ flows/VFK-151-Svar på høring - ordensregler for voksne.js
@@ -1,4 +1,4 @@
-const description = 'Svar på høring - lokal forskrift om skole- og feriedager 2026/27 og 2027/28'
+const description = 'Svar på høring - Ordensregler for voksne'
 const { nodeEnv } = require('../config')
 
 module.exports = {
@@ -6,8 +6,15 @@ module.exports = {
     enabled: true,
     doNotRemoveBlobs: false
   },
-  parseXml: {
-    enabled: true
+  parseJson: {
+    enabled: true,
+    options: {
+      mapper: (dialogueData) => {
+        // if (!dialogueData.Testskjema_for_?.Gruppa_øverst?.Fornavn) throw new Error('Missing Gruppa_øverst.Fornavn mangler i JSON filen')
+        return {
+        }
+      }
+    }
   },
 
   /* XML from Acos:
@@ -26,7 +33,7 @@ ArchiveData {
       mapper: (flowStatus) => { // for å opprette person basert på fødselsnummer
         // Mapping av verdier fra XML-avleveringsfil fra Acos.
         return {
-          ssn: flowStatus.parseXml.result.ArchiveData.fnr
+          ssn: flowStatus.parseJson.result.SavedValues.Login.UserID
         }
       }
     }
@@ -35,12 +42,12 @@ ArchiveData {
     enabled: true,
     options: {
       condition: (flowStatus) => { // use this if you only need to archive some of the forms.
-        return flowStatus.parseXml.result.ArchiveData.typeInnsender === 'Virksomhet'
+        return flowStatus.parseJson.result.DialogueInstance.Informasjon_om_.Hvem_represente === 'Organisasjon/virksomhet'
       },
       mapper: (flowStatus) => { // for å opprette person basert på fødselsnummer
         // Mapping av verdier fra XML-avleveringsfil fra Acos.
         return {
-          orgnr: flowStatus.parseXml.result.ArchiveData.orgnr.replaceAll(' ', '')
+          orgnr: flowStatus.parseJson.result.DialogueInstance.Informasjon_om_.Organisasjon.Organisasjonsnu.replaceAll(' ', '')
         }
       }
     }
@@ -51,7 +58,7 @@ ArchiveData {
     enabled: true,
     options: {
       mapper: (flowStatus, base64, attachments) => {
-        const xmlData = flowStatus.parseXml.result.ArchiveData
+        const jsonData = flowStatus.parseJson.result
         const p360Attachments = attachments.map(att => {
           return {
             Base64Data: att.base64,
@@ -69,7 +76,7 @@ ArchiveData {
             Contacts: [
               {
                 Role: 'Avsender',
-                ReferenceNumber: xmlData.typeInnsender === 'Privatperson' ? xmlData.fnr : xmlData.orgnr.replaceAll(' ', ''), // Hvis privatperson skal FNR benyttes, hvis ikke skal orgnr brukes
+                ReferenceNumber: jsonData.DialogueInstance.Informasjon_om_.Hvem_represente === 'Privatperson' ? jsonData.SavedValues.Login.UserID : jsonData.DialogueInstance.Informasjon_om_.Organisasjon.Organisasjonsnu.replaceAll(' ', ''), // Hvis privatperson skal FNR benyttes, hvis ikke skal orgnr brukes
                 IsUnofficial: false
               }
             ],
@@ -85,13 +92,13 @@ ArchiveData {
               },
               ...p360Attachments
             ],
-            ResponsibleEnterpriseRecno: nodeEnv === 'production' ? '200017' : '200020', // Seksjon Kompetanse og pedagogisk utvikling - Dette finner du i p360, ved å trykke "Avansert Søk" > "Kontakt" > "Utvidet Søk" > så søker du etter det du trenger Eks: "Søkenavn": %Idrett%. Trykk på kontakten og se etter org nummer.
-            ResponsiblePersonEmail: nodeEnv === 'production' ? 'jon.brekka@vestfoldfylke.no' : '',
+            ResponsibleEnterpriseRecno: nodeEnv === 'production' ? '200091' : '200148', // Seksjon Voksenopplæring og karriereutvikling - Dette finner du i p360, ved å trykke "Avansert Søk" > "Kontakt" > "Utvidet Søk" > så søker du etter det du trenger Eks: "Søkenavn": %Idrett%. Trykk på kontakten og se etter org nummer.
+            ResponsiblePersonEmail: nodeEnv === 'production' ? 'charlotte.viksand.glad@vestfoldfylke.no' : '',
             Status: 'J',
             AccessCode: 'U',
-            Title: 'Høringsinnspill - Lokal forskrift om skole- og feriedager 2026/2027 og 2027/2028',
+            Title: 'Høringsinnspill - Ordensregler for videregående opplæring for voksne i Vestfold fylkeskommune',
             Archive: 'Saksdokument',
-            CaseNumber: nodeEnv === 'production' ? '25/09080' : '25/00023'
+            CaseNumber: nodeEnv === 'production' ? '25/09173' : '25/00024'
           }
         }
       }
@@ -109,18 +116,31 @@ ArchiveData {
     enabled: true,
     options: {
       mapper: (flowStatus) => {
-        const xmlData = flowStatus.parseXml.result.ArchiveData
+        const jsonData = flowStatus.parseJson.result.DialogueInstance
         return [
           {
-            testListUrl: 'https://vestfoldfylke.sharepoint.com/sites/OPT-Fylkesadministrasjonopplring/Lists/Svar%20p%20hring%20%20skoleruta/AllItems.aspx',
-            prodListUrl: 'https://vestfoldfylke.sharepoint.com/sites/OPT-Fylkesadministrasjonopplring/Lists/Svar%20p%20hring%20%20skoleruta/AllItems.aspx',
+            testListUrl: 'https://vestfoldfylke.sharepoint.com/sites/OPT-Fylkesadministrasjonopplring/Lists/Svar%20p%20hring%20%20Ordensregler%20for%20voksne/AllItems.aspx',
+            prodListUrl: 'https://vestfoldfylke.sharepoint.com/sites/OPT-Fylkesadministrasjonopplring/Lists/Svar%20p%20hring%20%20Ordensregler%20for%20voksne/AllItems.aspx',
             uploadFormPdf: true,
             uploadFormAttachments: true,
             fields: {
-              Title: xmlData.navn, // husk å bruke internal name på kolonnen
-              Organisasjon: xmlData.organisasjon || 'Privatperson',
-              Innspill: xmlData.innspill,
-              Dokumentnummer_x0020_i_x0020_360: flowStatus.archive.result.DocumentNumber
+              Title: `${jsonData.Informasjon_om_.Innsender.Fornavn1} ${jsonData.Informasjon_om_.Innsender.Etternavn1}`, // husk å bruke internal name på kolonnen
+              Telefon: jsonData.Informasjon_om_.Innsender.Telefon1 || 'Privatperson',
+              E_x002d_post: jsonData.Informasjon_om_.Innsender.E_post,
+              Organisasjonsnavn: jsonData.Informasjon_om_.Organisasjon.Organisasjonsna || 'Privatperson',
+              Organisasjonsnummer: jsonData.Informasjon_om_.Organisasjon.Organisasjonsnu,
+              Hvorogn_x00e5_r: jsonData.Høring_og_hørin.Har_du_innspill,
+              Rettigheter: jsonData.Høring_og_hørin.Har_du_innspill6,
+              Plikter: jsonData.Høring_og_hørin.Har_du_innspill7,
+              N_x00e6_rv_x00e6_rogfrav_x00e6_r: jsonData.Høring_og_hørin.Har_du_innspill8,
+              Orden: jsonData.Høring_og_hørin.Har_du_innspill9,
+              Oppf_x00f8_rsel: jsonData.Høring_og_hørin.Har_du_innspill20,
+              Tiltakogreaksjoner: jsonData.Høring_og_hørin.Har_du_innspill17,
+              Rettigheterogplikter: jsonData.Høring_og_hørin.Har_du_innspill16,
+              Ersatning: jsonData.Høring_og_hørin.Har_du_innspill18,
+              Lokaleregler: jsonData.Høring_og_hørin.Har_du_innspill19,
+              Andrekommentarer: jsonData.Høring_og_hørin.Har_du_andre_in,
+              Dokumentnummeri360: flowStatus.archive.result.DocumentNumber
             }
           }
         ]
@@ -135,9 +155,9 @@ ArchiveData {
         // Mapping av verdier fra XML-avleveringsfil fra Acos. Alle properties under må fylles ut og ha verdier
         return {
           company: 'Opplæring og tannhelse',
-          department: 'Kompetanse og pedagogisk utvikling',
+          department: 'Seksjon Voksenopplæring og karriereutvikling',
           description,
-          type: 'Svar på høring - lokal forskrift om skole- og feriedager 2026/27 og 2027/28', // Required. A short searchable type-name that distinguishes the statistic element
+          type: 'Svar på høring - Ordensregler for voksne', // Required. A short searchable type-name that distinguishes the statistic element
           // optional fields:
           // tilArkiv: flowStatus.parseXml.result.ArchiveData.TilArkiv,
           documentNumber: flowStatus.archive?.result?.DocumentNumber || 'tilArkiv er false' // Optional. anything you like
