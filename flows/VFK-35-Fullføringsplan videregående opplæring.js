2cb3bfa6e2affa0b76bef6ae09c9b739cb9830f5
Nils Krane Thvedt <nilskt@gmail.com>
2025-06-03 09:03:20 +0200

    Update company and department fields in VFK-35 flow for further education plans

--- flows/VFK-35-Fullføringsplan videregående opplæring.js
+++ flows/VFK-35-Fullføringsplan videregående opplæring.js
@@ -151,8 +151,8 @@ module.exports = {
         // const xmlData = flowStatus.parseXml.result.ArchiveData
         // Mapping av verdier fra XML-avleveringsfil fra Acos. Alle properties under må fylles ut og ha verdier
         return {
-          company: 'Opplæring',
-          department: '',
+          company: 'Opplæring og tannhelse',
+          department: 'Kompetanse og pedagogisk utvikling',
           description,
           type: 'Fullføringsplan videregående opplæring', // Required. A short searchable type-name that distinguishes the statistic element
           // optional fields:
