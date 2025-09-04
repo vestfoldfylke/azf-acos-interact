d167d35d14323c0937847d879a9e14d58d2cf718
Nils Krane Thvedt <nilskt@gmail.com>
2025-09-04 07:48:36 +0200

    Update VFK-167 SharePoint list added

--- flows/VFK-167-Søknad om permisjon Videregående opplæring for voksne.js
+++ flows/VFK-167-Søknad om permisjon Videregående opplæring for voksne.js
@@ -4,7 +4,7 @@ const { schoolInfo } = require('../lib/data-sources/vfk-schools')
 module.exports = {
   config: {
     enabled: true,
-    doNotRemoveBlobs: false
+    doNotRemoveBlobs: true
   },
   parseJson: {
     enabled: true,
@@ -106,43 +106,31 @@ module.exports = {
   closeCase: {
     enabled: false
   },
-  /*
-    sharepointList: {
+
+  sharepointList: {
     enabled: true,
     options: {
       mapper: (flowStatus) => {
-        const xmlData = flowStatus.parseXml.result.ArchiveData
-        if (!xmlData.Postnr) throw new Error('Postnr har ikke kommet med fra XML')
+        const jsonData = flowStatus.parseJson.result
         return [
           {
-            siteId: '0a4121ce-7384-474c-afff-ee20f48bff5e',
-            path: 'sites/BDK-Jrgensteste-team/Lists/ACOS%20test%20%20Bestilling%20av%20dokumentasjon%20for%20privati/AllItems.aspx',
-            listId: 'D1085908-9111-4b6d-84d3-fc8ecd29d398',
+            testListUrl: 'https://vestfoldfylke.sharepoint.com/sites/OPT-Fylkesadministrasjonopplring-Tallogstatistikk/Lists/PermisjonVGOforvoksne/AllItems.aspx',
+            prodListUrl: 'https://vestfoldfylke.sharepoint.com/sites/OPT-Fylkesadministrasjonopplring-Tallogstatistikk/Lists/PermisjonVGOforvoksne/AllItems.aspx',
             uploadFormPdf: true,
             uploadFormAttachments: true,
             fields: {
-              Title: xmlData.Fnr || 'Mangler fnr', // husk å bruke internal name på kolonnen
-              Fornavn: xmlData.Fornavn,
-              Etternavn: xmlData.Etternavn,
-              Adresse: xmlData.Adresse,
-              Postnummerogsted: `${xmlData.Postnr} ${xmlData.Poststed}`,
-              Mobil: xmlData.Mobil,
-              E_x002d_postadresse: xmlData.Epost,
-              Typedokumentasjon: xmlData.TypeDok,
-              Typeautorasisjon: xmlData.TypeAut,
-              Eksamenssted: xmlData.Eksamenssted,
-              Fag: xmlData.Fag,
-              _x00d8_nsketmottak: xmlData.OnsketMottak,
-              Eksamensperiode: xmlData.AarSemester,
-              Alternativadresse: xmlData.AltAdresse
-
+              Title: `${jsonData.SavedValues.Login.FirstName} ${flowStatus.parseJson.result.SavedValues.Login.LastName}`,
+              Skole: jsonData.SavedValues.Dataset.Hvilke_skole_gå.Skole,
+              Permisjonstype: jsonData.DialogueInstance.Hvorfor_søker_d.Permisjonstype.Gjelder_søknade,
+              _x00c5_rsak: jsonData.DialogueInstance.Hvorfor_søker_d.Kortvarig_permi?.Grunn_for_permi || jsonData.DialogueInstance.Hvorfor_søker_d.Langvarig_permi.Grunn_for_permi1,
+              Kommentar: jsonData.DialogueInstance.Hvorfor_søker_d.Kortvarig_permi?.Kommentar || jsonData.DialogueInstance.Hvorfor_søker_d.Langvarig_permi.Kommentar1,
+              Dokumentnummeri360: flowStatus.archive?.result?.DocumentNumber || 'Ikke automatisk arkivert'
             }
           }
         ]
       }
     }
   },
-  */
 
   statistics: {
     enabled: true,
