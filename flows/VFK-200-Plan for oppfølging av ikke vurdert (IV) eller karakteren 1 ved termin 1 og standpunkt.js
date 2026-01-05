0c821b5acd64e36a041c92782ae62b70d2efb2d1
Nils Krane Thvedt <nilskt@gmail.com>
2026-01-05 09:49:12 +0100

    feat: add new flow for 'Plan for oppfølging av ikke vurdert (IV) eller karakteren 1 ved termin 1 og standpunkt'

---
+++ flows/VFK-200-Plan for oppfølging av ikke vurdert (IV) eller karakteren 1 ved termin 1 og standpunkt.js
@@ -0,0 +1,132 @@
+const description = 'Sender til elevmappe'
+const { schoolInfo } = require('../lib/data-sources/vfk-schools')
+// const { nodeEnv } = require('../config')
+module.exports = {
+  config: {
+    enabled: true,
+    doNotRemoveBlobs: false
+  },
+  parseJson: {
+    enabled: true,
+    options: {
+      mapper: (dialogueData) => {
+        // if (!dialogueData.Testskjema_for_?.Gruppa_øverst?.Fornavn) throw new Error('Missing Gruppa_øverst.Fornavn mangler i JSON filen')
+        return {
+        }
+      }
+    }
+  },
+
+  // Synkroniser elevmappe
+  syncElevmappe: {
+    enabled: true,
+    options: {
+      /*
+      condition: (flowStatus) => { // use this if you only need to archive some of the forms.
+        return flowStatus.parseXml.result.ArchiveData.TilArkiv === 'true'
+      },
+      */
+      mapper: (flowStatus) => { // for å opprette person basert på fødselsnummer
+        // Mapping av verdier fra XML-avleveringsfil fra Acos.
+        return {
+          // ssn: flowStatus.parseJson.result.SavedValues.Login.UserID
+          ssn: flowStatus.parseJson.result.DialogueInstance.Informasjon_om_.Elev.Fødselsnummer1
+        }
+      }
+    }
+  },
+
+  // Arkiverer dokumentet i elevmappa
+  archive: { // archive må kjøres for å kunne kjøre signOff (noe annet gir ikke mening)
+    enabled: true,
+    options: {
+      /*
+      condition: (flowStatus) => { // use this if you only need to archive some of the forms.
+        return flowStatus.parseXml.result.ArchiveData.TilArkiv === 'true'
+      },
+      */
+      mapper: (flowStatus, base64, attachments) => {
+        const elevmappe = flowStatus.syncElevmappe.result.elevmappe
+        const school = schoolInfo.find(school => school.orgNr.toString() === flowStatus.parseJson.result.SavedValues.Dataset.Skole1.Orgnr)
+        if (!school) throw new Error(`Could not find any school with orgNr: ${flowStatus.parseJson.result.SavedValues.Dataset.Skole1.Orgnr}`)
+        const p360Attachments = attachments.map(att => {
+          return {
+            Base64Data: att.base64,
+            Format: att.format,
+            Status: 'F',
+            Title: att.title,
+            VersionFormat: att.versionFormat
+          }
+        })
+        return {
+          service: 'DocumentService',
+          method: 'CreateDocument',
+          parameter: {
+            AccessCode: '13',
+            AccessGroup: flowStatus.parseJson.result.SavedValues.Dataset.Skole1.Tilgangsgruppe,
+            Category: 'Dokument inn',
+            Contacts: [
+              {
+                ReferenceNumber: flowStatus.parseJson.result.DialogueInstance.Informasjon_om_.Elev.Fødselsnummer1,
+                Role: 'Avsender',
+                IsUnofficial: true
+              }
+            ],
+            DocumentDate: new Date().toISOString(),
+            Files: [
+              {
+                Base64Data: base64,
+                Category: '1',
+                Format: 'pdf',
+                Status: 'F',
+                Title: 'Plan for oppfølging',
+                VersionFormat: 'A'
+              },
+              ...p360Attachments
+            ],
+            Paragraph: 'Offl. § 13 jf. fvl. § 13 (1) nr.1',
+            ResponsibleEnterpriseNumber: school.orgNr,
+            // ResponsiblePersonEmail: '',
+            Status: 'J',
+            Title: 'Plan for oppfølging av ikke vurdert (IV) eller karakteren 1 ved termin 1 og standpunkt',
+            UnofficialTitle: `Plan for oppfølging av ikke vurdert (IV) eller karakteren 1 ved termin 1 og standpunkt - ${flowStatus.parseJson.result.DialogueInstance.Informasjon_om_.Elev.Fornavn1} ${flowStatus.parseJson.result.DialogueInstance.Informasjon_om_.Elev.Etternavn1}`,
+            Archive: 'Elevdokument',
+            CaseNumber: elevmappe.CaseNumber
+          }
+        }
+      }
+    }
+
+  },
+
+  signOff: {
+    enabled: false
+  },
+
+  closeCase: {
+    enabled: false
+  },
+
+  statistics: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => {
+        // const xmlData = flowStatus.parseXml.result.ArchiveData
+        // Mapping av verdier fra XML-avleveringsfil fra Acos. Alle properties under må fylles ut og ha verdier
+        return {
+          company: 'Opplæring og tannhelse',
+          department: 'Seksjon kompetanse og pedagogisk utvikling',
+          description,
+          type: 'Plan for oppfølging av ikke vurdert (IV) eller karakteren 1 ved termin 1 og standpunkt', // Required. A short searchable type-name that distinguishes the statistic element
+          // optional fields:
+          // tilArkiv: flowStatus.parseXml.result.ArchiveData.TilArkiv,
+          documentNumber: flowStatus.archive?.result?.DocumentNumber || 'tilArkiv er false' // Optional. anything you like
+        }
+      }
+    }
+  },
+
+  failOnPurpose: {
+    enabled: false
+  }
+}
