c2701d79d7719b039c97271bdc9184e84757f98d
Nils Krane Thvedt <nilskt@gmail.com>
2026-01-09 08:12:28 +0100

    feat: add new flow for Liv og røre funding application. Replaces VFK0080

---
+++ flows/VFK-46-Søknad om tilskudd til Liv og røre på egen skole.js
@@ -0,0 +1,148 @@
+const description = 'Søknad om tilskudd til Liv og røre på egen skole'
+const { nodeEnv } = require('../config')
+
+module.exports = {
+  config: {
+    enabled: true,
+    doNotRemoveBlobs: false
+  },
+  parseJson: {
+    enabled: true,
+    options: {
+      mapper: (dialogueData) => {
+        // if (!dialogueData.Testskjema_for_?.Gruppa_øverst?.Fornavn) throw new Error('Missing Gruppa_øverst.Fornavn mangler i JSON filen')
+        return {
+        }
+      }
+    }
+  },
+
+  syncEnterprise: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => { // for å opprette/oppdatere en virksomhet i P3360
+        // Mapping av verdier fra XML-avleveringsfil fra Acos. Alle properties under må fylles ut og ha verdier for å opprette privatperson med fiktivt fødselsnummer
+        return {
+          orgnr: flowStatus.parseJson.result.DialogueInstance.Om_kommunen.Informasjon_om_.Organisasjon1.Organisasjonsnu.replaceAll(' ', '')
+        }
+      }
+    }
+  },
+
+  handleCase: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => {
+        return {
+          service: 'CaseService',
+          method: 'CreateCase',
+          parameter: {
+            CaseType: 'Sak',
+            Project: nodeEnv === 'production' ? '24-105' : '24-3',
+            Title: 'Søknad om tilskudd til Liv og røre', // - ${flowStatus.parseXml.result.ArchiveData.OmProsjektet}`,
+            // UnofficialTitle: ,
+            Status: 'B',
+            JournalUnit: 'Sentralarkiv',
+            SubArchive: 'Sakarkiv',
+            ArchiveCodes: [
+              {
+                ArchiveCode: '243',
+                ArchiveType: 'FELLESKLASSE PRINSIPP',
+                Sort: 1
+              }
+            ],
+            // ResponsibleEnterpriseRecno: nodeEnv === 'production' ? 'recno' : 'recno',
+            ResponsiblePersonEmail: 'eli.haraldsen@vestfoldfylke.no',
+            // AccessGroup: 'Alle'
+            AccessCode: 'U'
+          }
+        }
+      }
+    }
+  },
+  // Arkiverer dokumentet i 360
+  archive: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus, base64, attachments) => {
+        const caseNumber = flowStatus.handleCase.result.CaseNumber
+        // const caseNumber = nodeEnv === 'production' ? 'må fylles inn!' : '23/00115'
+        const p360Attachments = attachments.map(att => {
+          return {
+            Base64Data: att.base64,
+            Format: att.format,
+            Status: 'F',
+            Title: att.title,
+            VersionFormat: att.versionFormat
+          }
+        })
+        return {
+
+          service: 'DocumentService',
+          method: 'CreateDocument',
+          parameter: {
+            Category: 'Dokument inn',
+            Contacts: [
+              {
+                ReferenceNumber: flowStatus.parseJson.result.DialogueInstance.Om_kommunen.Informasjon_om_.Organisasjon1.Organisasjonsnu.replaceAll(' ', ''),
+                Role: 'Avsender',
+                IsUnofficial: false
+              }
+            ],
+            Files: [
+              {
+                Base64Data: base64,
+                Category: '1',
+                Format: 'pdf',
+                Status: 'F',
+                Title: 'Søknad om midler - Liv og røre',
+                VersionFormat: 'A'
+              },
+              ...p360Attachments
+            ],
+            Status: 'J',
+            DocumentDate: new Date().toISOString(),
+            Title: 'Søknad om midler - Liv og røre', // ` - ${flowStatus.parseXml.result.ArchiveData.OmProsjektet}`,
+            // UnofficialTitle: '',
+            Archive: 'Saksdokument',
+            CaseNumber: caseNumber,
+            // ResponsibleEnterpriseRecno: nodeEnv === 'production' ? 'recno-Prod' : 'recno-Test', // Seksjon samfunn og plan-  Dette finner du i p360, ved å trykke "Avansert Søk" > "Kontakt" > "Utvidet Søk" > så søker du etter det du trenger Eks: "Søkenavn": %Idrett%. Trykk på kontakten og se etter org nummer.
+            ResponsiblePersonEmail: 'eli.haraldsen@vestfoldfylke.no',
+            AccessCode: 'U'
+            // Paragraph: 'Offl. § 26 femte ledd',
+            // AccessGroup: ''
+          }
+        }
+      }
+    }
+
+  },
+
+  signOff: {
+    enabled: false
+  },
+
+  closeCase: {
+    enabled: false
+  },
+
+  statistics: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => {
+        // Mapping av verdier fra XML-avleveringsfil fra Acos. Alle properties under må fylles ut og ha verdier
+        return {
+          company: 'Samfunnsutvikling',
+          department: 'Seksjon samfunn og plan',
+          description, // Required. A description of what the statistic element represents
+          type: 'Søknad om midler - Liv og røre', // Required. A short searchable type-name that distinguishes the statistic element
+          // optional fields:
+          documentNumber: flowStatus.archive.result.DocumentNumber // Optional. anything you like
+        }
+      }
+    }
+  },
+  failOnPurpose: {
+    enabled: false
+  }
+}
