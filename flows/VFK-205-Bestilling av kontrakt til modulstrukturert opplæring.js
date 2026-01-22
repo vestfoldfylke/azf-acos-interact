9d0db89bdbe48401f93ad5f944b2fe02295ed53a
Nils Krane Thvedt <nilskt@gmail.com>
2026-01-22 08:00:00 +0100

    Add flow for document creation and synchronization for structured training orders

---
+++ flows/VFK-205-Bestilling av kontrakt til modulstrukturert opplæring.js
@@ -0,0 +1,131 @@
+const description = 'Sender til elevmappe'
+const { nodeEnv } = require('../config')
+module.exports = {
+  config: {
+    enabled: true,
+    doNotRemoveBlobs: false
+  },
+
+  parseJson: {
+    enabled: true,
+    options: {
+      mapper: (dialogueData) => {
+        // if (!dialogueData.Testskjema_for_?.Gruppa_øverst?.Fornavn) throw new Error('Missing Gruppa_øverst.Fornavn mangler i JSON filen')
+        return {
+        }
+      }
+    }
+  },
+
+  // Synkroniser elevmappe
+  syncElevmappe: {
+    enabled: true,
+    options: {
+      /*
+      condition: (flowStatus) => { // use this if you only need to archive some of the forms.
+        return flowStatus.parseXml.result.ArchiveData.TilArkiv === 'true'
+      },
+      */
+      mapper: (flowStatus) => { // for å opprette person basert på fødselsnummer
+        // Mapping av verdier fra XML-avleveringsfil fra Acos.
+        return {
+          ssn: flowStatus.parseJson.result.DialogueInstance.Informasjon_om_1.Kandidat_.Fødselsnummer_
+        }
+      }
+    }
+  },
+
+  // Arkiverer dokumentet i elevmappa
+  archive: {
+    enabled: true,
+    options: {
+      /*
+      condition: (flowStatus) => { // use this if you only need to archive some of the forms.
+        return flowStatus.parseXml.result.ArchiveData.TilArkiv === 'true'
+      },
+      */
+      mapper: (flowStatus, base64, attachments) => {
+        const elevmappe = flowStatus.syncElevmappe.result.elevmappe
+        const jsonData = flowStatus.parseJson.result.DialogueInstance
+        const navn = flowStatus.parseJson.result.DialogueInstance.Informasjon_om_1.Kandidat_.Fornavn_ + ' ' + flowStatus.parseJson.result.DialogueInstance.Informasjon_om_1.Kandidat_.Etternavn_
+        const p360Attachments = attachments.map(att => {
+          return {
+            Base64Data: att.base64,
+            Format: att.format,
+            Status: 'F',
+            Title: att.title,
+            VersionFormat: att.versionFormat
+          }
+        })
+        return {
+          service: 'DocumentService',
+          method: 'CreateDocument',
+          parameter: {
+            AccessCode: '13',
+            AccessGroup: 'Fagopplæring',
+            Category: 'Dokument inn',
+            Contacts: [
+              {
+                ReferenceNumber: jsonData.Informasjon_om_.Hvem_fyller_ut_ === 'Bedrift' ? jsonData.Informasjon_om_.Organisasjon.Organisasjonsnu : flowStatus.parseJson.result.SavedValues.Dataset.Navn_på_skole_.Orgnr,
+                Role: 'Avsender',
+                IsUnofficial: true
+              }
+            ],
+            DocumentDate: new Date().toISOString(),
+            Files: [
+              {
+                Base64Data: base64,
+                Category: '1',
+                Format: 'pdf',
+                Status: 'F',
+                Title: 'Bestilling av modulstrukturert opplæring',
+                VersionFormat: 'A'
+              },
+              ...p360Attachments
+            ],
+            Paragraph: 'Offl. § 13 jf. fvl. § 13 (1) nr.1',
+            ResponsibleEnterpriseRecno: nodeEnv === 'production' ? '200016' : '200019', // Seksjon Fag- og yrkesopplæring
+            // ResponsiblePersonEmail: '',
+            Status: 'J',
+            Title: 'Bestilling av modulstrukturert opplæring',
+            UnofficialTitle: `Bestilling av modulstrukturert opplæring - ${navn}`,
+            Archive: 'Elevdokument',
+            CaseNumber: elevmappe.CaseNumber
+          }
+        }
+      }
+    }
+
+  },
+
+  signOff: {
+    enabled: false
+  },
+
+  closeCase: {
+    enabled: false
+  },
+
+  statistics: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => {
+        // const xmlData = flowStatus.parseXml.result.ArchiveData
+        // Mapping av verdier fra XML-avleveringsfil fra Acos. Alle properties under må fylles ut og ha verdier
+        return {
+          company: 'Opplæring',
+          department: 'FAGOPPLÆRING',
+          description,
+          type: 'Bestilling av modulstrukturert opplæring', // Required. A short searchable type-name that distinguishes the statistic element
+          // optional fields:
+          // tilArkiv: flowStatus.parseXml.result.ArchiveData.TilArkiv,
+          documentNumber: flowStatus.archive?.result?.DocumentNumber // || 'tilArkiv er false' // Optional. anything you like
+        }
+      }
+    }
+  },
+
+  failOnPurpose: {
+    enabled: false
+  }
+}
