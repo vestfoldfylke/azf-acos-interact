e4d692fe3d671f184826f28bd970ba547822f036
Nils Krane Thvedt <nilskt@gmail.com>
2026-02-10 10:07:47 +0100

    Add flow for reporting scheme - Den kulturelle spaserstokken. Replaces VFK0078

---
+++ flows/VFK-44-Rapporteringsskjema - Den kulturelle spaserstokken.js
@@ -0,0 +1,116 @@
+const description = 'Rapporteringsskjema - Den kulturelle spaserstokken'
+const { nodeEnv } = require('../config')
+
+module.exports = {
+  config: {
+    enabled: true,
+    doNotRemoveBlobs: false
+  },
+
+  parseJson: {
+    enabled: true,
+    options: {
+      mapper: (dialogueData) => {
+        // if (!dialogueData.Testskjema_for_?.Gruppa_øverst?.Fornavn) throw new Error('Missing Gruppa_øverst.Fornavn mangler i JSON filen')
+        return {
+        }
+      }
+    }
+  },
+
+  syncEnterprise: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => { // for å opprette person basert på fødselsnummer
+        // Mapping av verdier fra XML-avleveringsfil fra Acos.
+        return {
+          orgnr: flowStatus.parseJson.result.DialogueInstance.Informasjon_om_kommunen.Om_kommunen.Organisasjon.Organisasjon_orgnr.replaceAll(' ', '')
+        }
+      }
+    }
+  },
+
+  // Arkiverer dokumentet i 360
+  archive: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus, base64, attachments) => {
+        const p360Attachments = attachments.map(att => {
+          return {
+            Base64Data: att.base64,
+            Format: att.format,
+            Status: 'F',
+            Title: att.title,
+            VersionFormat: att.versionFormat
+          }
+        })
+        return {
+
+          service: 'DocumentService',
+          method: 'CreateDocument',
+          parameter: {
+            Category: 'Dokument inn',
+            Contacts: [
+              {
+                ReferenceNumber: flowStatus.parseJson.result.DialogueInstance.Informasjon_om_kommunen.Om_kommunen.Organisasjon.Organisasjon_orgnr.replaceAll(' ', ''),
+                Role: 'Avsender',
+                IsUnofficial: false
+              }
+            ],
+            Files: [
+              {
+                Base64Data: base64,
+                Category: '1',
+                Format: 'pdf',
+                Status: 'F',
+                Title: 'Rapporteringsskjema - Den kulturelle spaserstokken',
+                VersionFormat: 'A'
+              },
+              ...p360Attachments
+            ],
+            Status: 'J',
+            DocumentDate: new Date().toISOString(),
+            Title: 'Rapporteringsskjema - Den kulturelle spaserstokken',
+            // UnofficialTitle: '',
+            Archive: 'Saksdokument',
+            CaseNumber: nodeEnv === 'production' ? '26/04897' : '24/00027',
+            // ResponsibleEnterpriseRecno: nodeEnv === 'production' ? '200025' : '200031', // Seksjon Kultur Dette finner du i p360, ved å trykke "Avansert Søk" > "Kontakt" > "Utvidet Søk" > så søker du etter det du trenger Eks: "Søkenavn": %Idrett%. Trykk på kontakten og se etter org nummer.
+            ResponsiblePersonEmail: nodeEnv === 'production' ? 's.fosterpilkington@vestfoldfylke.no' : 'nils.thvedt@vestfoldfylke.no',
+            AccessCode: 'U'
+            // Paragraph: 'Offl. § 26 femte ledd',
+            // AccessGroup: 'Seksjon Kulturarv'
+          }
+        }
+      }
+    }
+
+  },
+
+  signOff: {
+    enabled: false
+  },
+
+  closeCase: {
+    enabled: false
+  },
+
+  statistics: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => {
+        // Mapping av verdier fra XML-avleveringsfil fra Acos. Alle properties under må fylles ut og ha verdier
+        return {
+          company: 'Samfunnsutvikling',
+          department: 'Kultur',
+          description, // Required. A description of what the statistic element represents
+          type: 'Rapporteringsskjema - Den kulturelle spaserstokken', // Required. A short searchable type-name that distinguishes the statistic element
+          // optional fields:
+          documentNumber: flowStatus.archive.result.DocumentNumber // Optional. anything you like
+        }
+      }
+    }
+  },
+  failOnPurpose: {
+    enabled: false
+  }
+}
