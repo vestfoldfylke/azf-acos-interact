c5066c246df5c034550bff4a6d9806db4dcc1efd
Nils Krane Thvedt <nilskt@gmail.com>
2026-02-23 12:57:14 +0100

    Fix data mapping for 'Konsekvenser_av' in recruitment needs flow

--- flows/VFK-150-Innmelding av rekrutteringsbehov til bemanningsrådet.js
+++ flows/VFK-150-Innmelding av rekrutteringsbehov til bemanningsrådet.js
@@ -49,7 +49,7 @@ module.exports = {
               Beskrivelse: jsonData.Informasjon_om_.Beskrivelse_av_,
               Prioritet: jsonData.Informasjon_om_.Prioritet,
               Finansiering: jsonData.Informasjon_om_.Finansiering,
-              Konskv_x002e_avikkeansettelse: jsonData.Konsekvenser_av,
+              Konskv_x002e_avikkeansettelse: jsonData.Informasjon_om_.Konsekvenser_av,
               Evt_x002e_kommentar: jsonData.Informasjon_om_.Eventuelle_komm,
               Telefon: jsonData.Informasjon_om_.Innsender.Telefon1,
               E_x002d_post0: jsonData.Informasjon_om_.Innsender.E_post
