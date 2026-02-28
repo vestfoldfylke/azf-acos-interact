bae5c9f1f7118b9ef2606996133756cfe0abbb4c
Nils Krane Thvedt <nilskt@gmail.com>
2026-02-28 08:47:39 +0100

    Add new flow for 'Høring - navneforslag på ny videregående skole i Holmestrand kommune'

---
+++ flows/VFK-213-Høring - navneforslag på ny videregående skole i Holmestrand kommune.js
@@ -0,0 +1,54 @@
+const description = 'Høring - navneforslag på ny videregående skole i Holmestrand kommune'
+module.exports = {
+  config: {
+    enabled: true,
+    doNotRemoveBlobs: false
+  },
+
+  parseJson: {
+    enabled: true,
+    options: {
+      mapper: (dialogueData) => {
+        // if (!dialogueData.Testskjema_for_?.Gruppa_øverst?.Fornavn) throw new Error('Missing Gruppa_øverst.Fornavn mangler i JSON filen')
+        return {
+        }
+      }
+    }
+  },
+
+  sharepointList: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => {
+        const jsonData = flowStatus.parseJson.result.DialogueInstance.Gi_din_stemme
+        return [
+          {
+            testListUrl: 'https://vestfoldfylke.sharepoint.com/sites/NavneprosessnyVGS/Lists/Hringsinnspill/AllItems.aspx',
+            prodListUrl: 'https://vestfoldfylke.sharepoint.com/sites/NavneprosessnyVGS/Lists/Hringsinnspill/AllItems.aspx',
+            uploadFormPdf: true,
+            uploadFormAttachments: false,
+            fields: {
+              Title: `${jsonData.Innsender.Fornavn1} ${jsonData.Innsender.Etternavn1}` || 'Mangler navn', // husk å bruke internal name på kolonnen
+              Navnforslag: jsonData.Les_kriteriene_
+            }
+          }
+        ]
+      }
+    }
+  },
+  statistics: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => {
+        // Mapping av verdier fra XML-avleveringsfil fra Acos. Alle properties under må fylles ut og ha verdier
+        return {
+          company: 'Opplæring',
+          department: 'Seksjon Sektorstøtte, inntak og eksamen',
+          description, // Required. A description of what the statistic element represents
+          type: 'Høring - navneforslag på ny videregående skole i Holmestrand kommune' // Required. A short searchable type-name that distinguishes the statistic element
+          // optional fields:
+        }
+      }
+    }
+  }
+}
