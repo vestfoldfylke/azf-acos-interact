7f41d076a43ccc506a33965d47de2d3befb0862e
Nils Krane Thvedt <nilskt@gmail.com>
2026-03-24 08:50:17 +0100

    feat: Add flow for handling innplasseringssamtale process

---
+++ flows/VFK-222-Skjema for innplasseringssamtale.js
@@ -0,0 +1,174 @@
+const description = 'Innplasseringssamtale'
+module.exports = {
+  config: {
+    enabled: true,
+    doNotRemoveBlobs: false
+  },
+
+  parseJson: {
+    enabled: true,
+    options: {
+      mapper: (dialogueData) => {
+        // if (!dialogueData.Testskjema_for_?.Gruppa_øverst?.Fornavn) throw new Error('Missing Gruppa_øverst.Fornavn mangler i JSON filen')
+        return {
+        }
+      }
+    }
+  },
+
+  syncEmployee: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => { // for å opprette person med fiktivt fødselsnummer
+        // Mapping av verdier fra XML-avleveringsfil fra Acos. Alle properties under må fylles ut og ha verdier for å opprette privatperson med fiktivt fødselsnummer
+        return {
+          ssn: flowStatus.parseJson.result.SavedValues.Integration.Hent_manuell_entra_bruker.extension_0fe49c4c681d427aa4cad2252aba12f5_employeeNumber,
+          forceUpdate: false // optional - forces update of privatePerson instead of quick return if it exists
+        }
+      }
+    }
+  },
+
+  handleCase: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => {
+        return {
+          service: 'CaseService',
+          method: 'CreateCase',
+          parameter: {
+            CaseType: 'Personal',
+            // Project: '20-15',
+            Title: 'Innplasseringssamtale',
+            UnofficialTitle: `Innplasseringssamtale - ${flowStatus.parseJson.result.SavedValues.Integration.Hent_manuell_entra_bruker.displayName}`,
+            Status: 'A',
+            AccessCode: '13',
+            Paragraph: 'Offl. § 13 jf. fvl. § 13 (1) nr.1',
+            JournalUnit: 'Sentralarkiv',
+            SubArchive: 'Personal',
+            ArchiveCodes: [
+              {
+                ArchiveCode: '420',
+                ArchiveType: 'FELLESKLASSE PRINSIPP',
+                Sort: 2
+              },
+              {
+                ArchiveCode: flowStatus.parseJson.result.SavedValues.Integration.Hent_manuell_entra_bruker.extension_0fe49c4c681d427aa4cad2252aba12f5_employeeNumber,
+                ArchiveType: 'FNR',
+                Sort: 1,
+                IsManualText: true
+              }
+            ],
+            Contacts: [
+              {
+                Role: 'Sakspart',
+                ReferenceNumber: flowStatus.parseJson.result.SavedValues.Integration.Hent_manuell_entra_bruker.extension_0fe49c4c681d427aa4cad2252aba12f5_employeeNumber,
+                IsUnofficial: true
+              }
+            ],
+            ResponsibleEnterpriseRecno: flowStatus.syncEmployee.result.responsibleEnterprise.recno,
+            ResponsiblePersonEmail: flowStatus.syncEmployee.result.archiveManager.email,
+            AccessGroup: '' // Automatisk
+          }
+        }
+      }
+    }
+  },
+
+  archive: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus, base64, attachments) => {
+        const p360Attachments = attachments.map(att => {
+          return {
+            Base64Data: att.base64,
+            Format: att.format,
+            Status: 'F',
+            Title: att.title,
+            VersionFormat: att.versionFormat
+          }
+        })
+        return {
+          service: 'DocumentService',
+          method: 'CreateDocument',
+          parameter: {
+            AccessCode: '13',
+            // AccessGroup: '', Automatisk tilgangsgruppe
+            Category: 'Dokument inn',
+            Contacts: [
+              {
+                ReferenceNumber: flowStatus.parseJson.result.SavedValues.Integration.Hent_manuell_entra_bruker.extension_0fe49c4c681d427aa4cad2252aba12f5_employeeNumber,
+                Role: 'Avsender',
+                IsUnofficial: true
+              }
+              /*,
+              {
+                ReferenceNumber: `recno: ${flowStatus.syncEmployee.result.archiveManager.recno}`,
+                Role: 'Mottaker'
+              }
+              */
+            ],
+            /*
+            UnregisteredContacts: [
+              {
+                ContactName: flowStatus.parseJson.result.DialogueInstance.Rekvisisjon_til.Godkjenning_fra.Navn1,
+                Role: 'Saksbehandler'
+              }
+            ],
+            */
+            DocumentDate: new Date().toISOString(),
+            Files: [
+              {
+                Base64Data: base64,
+                Category: '1',
+                Format: 'pdf',
+                Status: 'F',
+                Title: 'Innplasseringssamtale',
+                VersionFormat: 'A'
+              },
+              ...p360Attachments
+            ],
+            Paragraph: 'Offl. § 13 jf. fvl. § 13 (1) nr.1',
+            ResponsibleEnterpriseRecno: flowStatus.syncEmployee.result.responsibleEnterprise.recno,
+            ResponsiblePersonEmail: flowStatus.syncEmployee.result.archiveManager.email,
+            Status: 'J',
+            Title: 'Innplasseringssamtale',
+            UnofficialTitle: `Innplasseringssamtale - ${flowStatus.parseJson.result.SavedValues.Integration.Hent_manuell_entra_bruker.displayName}`,
+            Archive: 'Personaldokument',
+            CaseNumber: flowStatus.handleCase.result.CaseNumber
+          }
+        }
+      }
+    }
+  },
+
+  signOff: {
+    enabled: true
+  },
+
+  closeCase: { // handleCase må kjøres for å kunne kjøre closeCase
+    enabled: true
+  },
+
+  statistics: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => {
+        // Mapping av verdier fra XML-avleveringsfil fra Acos. Alle properties under må fylles ut og ha verdier
+        return {
+          company: 'HR',
+          department: '',
+          description,
+          type: 'Innplasseringssamtale',
+          // Required. A short searchable type-name that distinguishes the statistic element
+          // optional fields:
+          responsibleEnterprise: flowStatus.syncEmployee.result.responsibleEnterprise.shortName
+        }
+      }
+    }
+  },
+
+  failOnPurpose: {
+    enabled: false
+  }
+}
