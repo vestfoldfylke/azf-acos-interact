ee7b5ca98784faa1715c7bff7ebfa5c1af878027
Nils Krane Thvedt <nilskt@gmail.com>
2026-08-11 11:43:45 +0200

    fix: add flow for elevmappe handling and document archiving

---
+++ flows/VFK-241-Forhåndsgodkjenning av fraværsårsak.js
@@ -0,0 +1,134 @@
+const description = "Sender til elevmappe"
+// const { nodeEnv } = require('../config')
+const { schoolInfo } = require("../lib/data-sources/vfk-schools")
+
+module.exports = {
+  config: {
+    enabled: true,
+    doNotRemoveBlobs: false
+  },
+
+  parseJson: {
+    enabled: true,
+    options: {
+      mapper: (_dialogueData) => {
+        // if (!dialogueData.Testskjema_for_?.Gruppa_øverst?.Fornavn) throw new Error('Missing Gruppa_øverst.Fornavn mangler i JSON filen')
+        return {}
+      }
+    }
+  },
+
+  // Synkroniser elevmappe
+  syncElevmappe: {
+    enabled: true,
+    options: {
+      /*
+      condition: (flowStatus) => { // use this if you only need to archive some of the forms.
+        return flowStatus.parseXml.result.ArchiveData.TilArkiv === 'true'
+      },
+      */
+      mapper: (flowStatus) => {
+        // for å opprette person basert på fødselsnummer
+        // Mapping av verdier fra XML-avleveringsfil fra Acos.
+        return {
+          ssn: flowStatus.parseJson.result.DialogueInstance.Informasjon_om_.Elev.Fødselsnummer
+        }
+      }
+    }
+  },
+
+  // Arkiverer dokumentet i elevmappa
+  archive: {
+    // archive må kjøres for å kunne kjøre signOff (noe annet gir ikke mening)
+    enabled: true,
+    options: {
+      /*
+      condition: (flowStatus) => { // use this if you only need to archive some of the forms.
+        return flowStatus.parseXml.result.ArchiveData.TilArkiv === 'true'
+      },
+      */
+      mapper: (flowStatus, base64, attachments) => {
+        const jsonData = flowStatus.parseJson.result
+        const elevmappe = flowStatus.syncElevmappe.result.elevmappe
+        const school = schoolInfo.find((school) => school.companyName === jsonData.SavedValues.Integration.Hent_data_om_elev_fra_fra.companyName)
+        if (!school) throw new Error(`Could not find any school with name: ${jsonData.SavedValues.Integration.Hent_data_om_elev_fra_fra.companyName}`)
+        const p360Attachments = attachments.map((att) => {
+          return {
+            Base64Data: att.base64,
+            Format: att.format,
+            Status: "F",
+            Title: att.title,
+            VersionFormat: att.versionFormat
+          }
+        })
+        return {
+          service: "DocumentService",
+          method: "CreateDocument",
+          parameter: {
+            AccessCode: "13",
+            AccessGroup: school.tilgangsgruppe,
+            Category: "Dokument inn",
+            Contacts: [
+              {
+                ReferenceNumber: flowStatus.parseJson.result.DialogueInstance.Informasjon_om_.Elev.Fødselsnummer,
+                Role: "Avsender",
+                IsUnofficial: true
+              }
+            ],
+            DocumentDate: new Date().toISOString(),
+            Files: [
+              {
+                Base64Data: base64,
+                Category: "1",
+                Format: "pdf",
+                Status: "F",
+                Title: "Kontaktinformasjon",
+                VersionFormat: "A"
+              },
+              ...p360Attachments
+            ],
+            Paragraph: "Offl. § 13 jf. fvl. § 13 (1) nr.1",
+            ResponsibleEnterpriseNumber: school.orgNr,
+            // ResponsiblePersonEmail: '',
+            Status: "J",
+            Title: "Forhåndsgodkjenning av fraværsårsak",
+            // UnofficialTitle: `Kontaktinformasjon - ${xmlData.Fornavn} ${xmlData.Etternavn}`,
+            Archive: "Sensitivt elevdokument",
+            CaseNumber: elevmappe.CaseNumber
+          }
+        }
+      }
+    }
+  },
+
+  signOff: {
+    enabled: false
+  },
+
+  closeCase: {
+    enabled: false
+  },
+  
+  statistics: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => {
+        // const xmlData = flowStatus.parseXml.result.ArchiveData
+        // Mapping av verdier fra XML-avleveringsfil fra Acos. Alle properties under må fylles ut og ha verdier
+        return {
+          company: "Opplæring",
+          department: "",
+          description,
+          type: "Forhåndsgodkjenning av fraværsårsak", // Required. A short searchable type-name that distinguishes the statistic element
+          // optional fields:
+          // tilArkiv: flowStatus.parseXml.result.ArchiveData.TilArkiv,
+          documentNumber: flowStatus.archive?.result?.DocumentNumber //  || 'tilArkiv er false' // Optional. anything you like
+        }
+      }
+    }
+  },
+
+  failOnPurpose: {
+    enabled: false
+  }
+}
