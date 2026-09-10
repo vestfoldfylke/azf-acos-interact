377335e511e602773e91bee22d32c9d1ae7b4081
Nils Krane Thvedt <nilskt@gmail.com>
2026-09-10 10:10:24 +0200

    fix: update ResponsibleEnterpriseRecno for various flows to reflect new organizational structure

--- flows/VFK-18-Prioritert gjesteelev i Vestfold.js
+++ flows/VFK-18-Prioritert gjesteelev i Vestfold.js
@@ -82,7 +82,7 @@ module.exports = {
               ...p360Attachments
             ],
             Paragraph: "Offl. § 13 jf. fvl. § 13 (1) nr.1",
-            ResponsibleEnterpriseRecno: nodeEnv === "production" ? "200015" : "200018", // Seksjon Sektorstøtte, inntak og eksamen
+            ResponsibleEnterpriseRecno: nodeEnv === "production" ? "262222" : "200735", // Team inntak
             // ResponsiblePersonEmail: '',
             Status: "J",
             Title: "Prioritert gjesteelev",
