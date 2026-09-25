c99acdf3417c46578c01bb0bafd2c3c1667bbe5a
Nils Krane Thvedt <nilskt@gmail.com>
2026-09-25 10:57:46 +0200

    fix: remove syncPrivatePerson mapping and update syncEnterprise to use correct organization number and new SharePointlist

--- flows/VFK-180-Søknad om tilskudd til kunstproduksjon.js
+++ flows/VFK-180-Søknad om tilskudd til kunstproduksjon.js
@@ -17,33 +17,12 @@ module.exports = {
     }
   },
 
-  syncPrivatePerson: {
-    enabled: true,
-    options: {
-      condition: (flowStatus) => {
-        // use this if you only need to archive some of the forms.
-        return flowStatus.parseJson.result.DialogueInstance.Informasjon_om_.Type_søker.Jeg_søker_på_ve === "meg selv (som privatperson)"
-      },
-      mapper: (flowStatus) => {
-        // for å opprette person basert på fødselsnummer
-        // Mapping av verdier fra XML-avleveringsfil fra Acos.
-        return {
-          ssn: flowStatus.parseJson.result.SavedValues.Login.UserID
-        }
-      }
-    }
-  },
-
   syncEnterprise: {
     enabled: true,
     options: {
-      condition: (flowStatus) => {
-        // use this if you only need to archive some of the forms.
-        return flowStatus.parseJson.result.DialogueInstance.Informasjon_om_.Type_søker.Jeg_søker_på_ve === "en organisasjon"
-      },
       mapper: (flowStatus) => {
         return {
-          orgnr: flowStatus.parseJson.result.DialogueInstance.Informasjon_om_.Organisasjon1.Organisasjon.Organisasjonsnu.replaceAll(" ", "")
+          orgnr: flowStatus.parseJson.result.DialogueInstance.Informasjon_om_.Organisasjon1.Organisasjon2.Organisasjonsnummer.replaceAll(" ", "")
         }
       }
     }
@@ -71,10 +50,7 @@ module.exports = {
             Contacts: [
               {
                 Role: "Avsender",
-                ReferenceNumber:
-                  jsonData.Informasjon_om_.Type_søker.Jeg_søker_på_ve === "meg selv (som privatperson)"
-                    ? flowStatus.parseJson.result.SavedValues.Login.UserID
-                    : jsonData.Informasjon_om_.Organisasjon1.Organisasjon.Organisasjonsnu.replaceAll(" ", ""),
+                ReferenceNumber: jsonData.Informasjon_om_.Organisasjon1.Organisasjon2.Organisasjonsnummer.replaceAll(" ", ""),
                 IsUnofficial: false
               }
             ],
@@ -93,7 +69,7 @@ module.exports = {
             DocumentDate: new Date().toISOString(),
             Title: `Søknad om tilskudd til kunstproduksjon - ${jsonData.Beskrivelse.Tilskuddsordnin.Velg_tilskuddso}`,
             Archive: "Saksdokument",
-            CaseNumber: nodeEnv === "production" ? "25/15698" : "24/00066",
+            CaseNumber: nodeEnv === "production" ? "26/14100" : "24/00066",
             ResponsibleEnterpriseRecno: nodeEnv === "production" ? "200025" : "200031", // Seksjon Kultur Dette finner du i p360, ved å trykke "Avansert Søk" > "Kontakt" > "Utvidet Søk" > så søker du etter det du trenger Eks: "Søkenavn": %Idrett%. Trykk på kontakten og se etter org nummer.
             ResponsiblePersonEmail: nodeEnv === "production" ? "yvonne.pleym@vestfoldfylke.no" : "jorn.roger.skaugen@vestfoldfylke.no",
             AccessCode: "U"
@@ -118,25 +94,28 @@ module.exports = {
         const jsonData = flowStatus.parseJson.result.DialogueInstance
         return [
           {
-            testListUrl: "https://vestfoldfylke.sharepoint.com/sites/V-Samfunnsutvikling/Lists/Sknad%20om%20sttte%20til%20kunstproduksjon%20%202025/AllItems.aspx",
-            prodListUrl: "https://vestfoldfylke.sharepoint.com/sites/V-Samfunnsutvikling/Lists/Sknad%20om%20sttte%20til%20kunstproduksjon%20%202025/AllItems.aspx",
+            testListUrl: "https://vestfoldfylke.sharepoint.com/sites/V-Samfunnsutvikling/Lists/Kunstproduksjon%20%202027/AllItems.aspx",
+            prodListUrl: "https://vestfoldfylke.sharepoint.com/sites/V-Samfunnsutvikling/Lists/Kunstproduksjon%20%202027/AllItems.aspx",
             uploadFormPdf: true,
             uploadFormAttachments: true,
             fields: {
-              Title:
-                jsonData.Informasjon_om_.Type_søker.Jeg_søker_på_ve === "meg selv (som privatperson)"
-                  ? `${flowStatus.parseJson.result.SavedValues.Login.FirstName} ${flowStatus.parseJson.result.SavedValues.Login.LastName}`
-                  : `${jsonData.Informasjon_om_.Organisasjon1.Organisasjon.Organisasjonsna} - ${jsonData.Informasjon_om_.Organisasjon1.Kontaktperson}`,
+              Title: `${jsonData.Informasjon_om_.Organisasjon1.Organisasjon2.Organisasjonsnavn ?? ""} - ${jsonData.Informasjon_om_.Organisasjon1.Organisasjon2.Organisasjonsnummer ?? ""}`,
               Tilskuddsordning: jsonData.Beskrivelse.Tilskuddsordnin.Velg_tilskuddso,
               Prosjektnavn: jsonData.Beskrivelse.Prosjektbeskriv.Navn_på_prosjek,
-              Prosjektbeskrivelse: jsonData.Beskrivelse.Prosjektbeskriv.Kort_beskrivels,
+              Kort_x0020_beskrivelse: jsonData.Beskrivelse.Prosjektbeskriv.Kort_beskrivels,
+              Prosjektbeskrivelse: jsonData.Beskrivelse.Prosjektbeskriv.Beskrivelse_av,
               Samarbeidspartnere: jsonData.Beskrivelse.Prosjektbeskriv.Samarbeidspartn,
+              Bidrag_x0020_til_x0020_mangfold_: jsonData.Beskrivelse.Prosjektbeskriv.Hvordan_kan,
               Prosjektperiode_x0020_fra: jsonData.Beskrivelse.Prosjektbeskriv.Prosjekperiode_,
               Prosjektperiode_x0020_til: jsonData.Beskrivelse.Prosjektbeskriv.Prosjektperiode,
+              Kulturr_x00e5_det: `${jsonData.Økonomi.Tilskudd_fra.Mottar_dere ?? ""} - ${jsonData.Økonomi.Tilskudd_fra.Hvilken_ordning ?? ""}`,
               S_x00f8_knadssum: jsonData.Økonomi.Oppsummering.Søknadssum,
-              Totalkostnad: jsonData.Økonomi.Oppsummering.Sum_utgifter,
               Andre_x0020_inntekter: jsonData.Økonomi.Oppsummering.Andre_inntekter1,
-              Dokumentnummer_x0020_i_x0020_360: flowStatus.archive.result.DocumentNumber
+              Tilskudd_x0020_kulturr_x00e5_det: jsonData.Økonomi.Oppsummering.Tilskudd_fra1,
+              Sum_x0020_inntekter: jsonData.Økonomi.Oppsummering.Sum_inntekter,
+              Totalkostnad: jsonData.Økonomi.Oppsummering.Sum_utgifter,
+              Dokumentnummer_x0020_i_x0020_360: flowStatus.archive.result.DocumentNumber,
+              Acos_x0020_refId: flowStatus.parseJson.result.Metadata.ReferenceId.Value
             }
           }
         ]
